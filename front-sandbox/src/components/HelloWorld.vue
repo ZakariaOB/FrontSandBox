a9e4591ad00b6854215974b1329b56d0c843f695
ZakariaOB <boukhriszakaria@gmail.com>
2021-06-19 19:17:22 +0100

    First commit

---
+++ front-sandbox/src/components/HelloWorld.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="hello">
+    <h3>FRONT SAMPLES</h3>
+    <ul>
+      <li>
+        <a
+          href="/sandbox/Bootstrap4/0_boostratp_sandbox/SandBoxSamples/2_2_basic_typography.html"
+          target="_blank"
+          rel="noopener"
+          >Bootstrap sandbox</a
+        >
+      </li>
+      <li>
+        <a href="/sandbox/Jo/Natours/index.html" target="_blank" rel="noopener"
+          >Natours</a
+        >
+      </li>
+      <li>
+        <a
+          href="/sandbox/Jo/Trillo_FelxBox/index.html"
+          target="_blank"
+          rel="noopener"
+          >Trillo</a
+        >
+      </li>
+      <li>
+        <a
+          href="/sandbox/Bootstrap4/3_LoopLab/index.html"
+          target="_blank"
+          rel="noopener"
+          >LoopLab</a
+        >
+      </li>
+      <li>
+        <a
+          href="/sandbox/Bootstrap4/4_Mizuxe/index.html"
+          target="_blank"
+          rel="noopener"
+          >Mizuxe</a
+        >
+      </li>
+      <li>
+        <a
+          href="/sandbox/Bootstrap4/6_Glozom/index.html"
+          target="_blank"
+          rel="noopener"
+          >Glozom</a
+        >
+      </li>
+      <li>
+        <a
+          href="/sandbox/Bootstrap4/7_blogen/index.html"
+          target="_blank"
+          rel="noopener"
+          >Blogen</a
+        >
+      </li>
+      <li>
+        <a
+          href="/sandbox/Bootstrap4/8_portfoilogrid/index.html"
+          target="_blank"
+          rel="noopener"
+          >Portfoli Grid</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
